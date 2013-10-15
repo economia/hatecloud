340ec0534bbff0adae25969be2f3f774338cf96d
Marcel Šulek <marcel@sulek.eu>
2013-10-15 10:58:19 +0200

    updatnut loader externich skriptu na sync verzi

--- www/js/_loadExternal.js
+++ www/js/_loadExternal.js
@@ -1,6 +1,13 @@
-var ext = document.createElement('script');
-ext.type = 'text/javascript';
-ext.async = false;
-ext.src = './external.js';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(ext, s);
+(function() {
+// based on http://stackoverflow.com/questions/2879509/dynamically-loading-javascript-synchronously
+// get some kind of XMLHttpRequest
+var xhrObj = (new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHttp"))
+// open and send a synchronous request
+xhrObj.open('GET', "./external.js", false);
+xhrObj.send('');
+// add the returned content to a newly created script tag
+var se = document.createElement('script');
+se.type = "text/javascript";
+se.text = xhrObj.responseText;
+document.getElementsByTagName('head')[0].appendChild(se);
+}());
